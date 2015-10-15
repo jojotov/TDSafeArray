f9bc89f8933871b8e4a7942af0be5c4180caab3c
tongzhou <tongzhou>
2015-10-15 12:53:29 +0800

    Initial Commit

---
+++ TDSafeArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TDSafeArray
+//
+//  Created by jojoting on 15/10/15.
+//  Copyright © 2015年 jojoting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
